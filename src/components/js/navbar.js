7bfa7f135e5dce82cb8e18d74f00e5673fcdf798
srinivaslikhith <likhithsrinivas9@gmail.com>
2024-08-23 23:19:01 -0500

    fix mobile issues

--- src/components/js/navbar.js
+++ src/components/js/navbar.js
@@ -1,14 +1,24 @@
-import { Link } from 'wouter';
-import '../css/navbar.css'
+import { Link, useLocation } from 'wouter';
+import '../css/navbar.css';
 
-function Navbar(){
-    return(
+function Navbar() {
+    const [location] = useLocation();
+
+    return (
         <nav>
             <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/projects'>Projects</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
+                <li>
+                    <Link href="/" className={location === '/' ? 'active' : ''}>Home</Link>
+                </li>
+                <li>
+                    <Link href="/about" className={location === '/about' ? 'active' : ''}>About</Link>
+                </li>
+                <li>
+                    <Link href="/projects" className={location === '/projects' ? 'active' : ''}>Projects</Link>
+                </li>
+                <li>
+                    <Link href="/contact" className={location === '/contact' ? 'active' : ''}>Contact</Link>
+                </li>
             </ul>
         </nav>
     );
